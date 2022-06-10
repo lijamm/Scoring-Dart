3f8b5cb0f48a261e0febe5fbaba98af405c888fc
lijamm <lizamalia28@gmail.com>
2022-06-10 15:14:52 +0800

    Scoring dart

---
+++ bin/nulll.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+void main (){
+  stdout.write('Input nilai dari 1-100:');
+  var score = num.parse(stdin.readLineSync()!);
+  print('nilai anda ${calculateScore(score)}');
+
+}
+String calculateScore (num score){
+if (score>90){
+  return 'A';
+}
+else if ( score>80){
+  return 'B';
+}
+else if (score > 70){
+  return 'C';
+}
+else{
+  return 'D';
+}
+}
+
+
